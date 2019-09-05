7fd63378e1edfba902419ca632d317f45978d596
Ramko9999 <ramapitchala@gmail.com>
2019-09-04 23:11:31 -0500

    Breadth First Search Working! DFS is not ideal

--- src/PathFinder.java
+++ src/PathFinder.java
@@ -1,6 +1,19 @@
+
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
+//a class that contains different algorithms to search a grid to find the optimal path
+
+/*
+20 represents the end point
+10 represents the start point
+-5 represents nodes or locations that have been calculated
+1 represents nodes or locations that are being examined
+-1 represents a wall or a barrier
+
+ */
 public class PathFinder {
 
     int [][] board;
@@ -20,11 +33,9 @@ public class PathFinder {
     }
 
     public static void main(String[] args) {
-        PathFinder pathFinder =  new PathFinder(new int[12][12], 1, 1, 9, 10);
+        PathFinder pathFinder =  new PathFinder(new int[12][12], 7, 0, 9, 10);
         pathFinder.A_Star_Search();
-        System.out.println("Trying DFS");
-        System.out.println("------------------------");
-        pathFinder.DFS();
+        pathFinder.BFS();
 
     }
 
@@ -34,12 +45,6 @@ public class PathFinder {
             newArray[i] = array[i].clone();
         }
         return newArray;
-    }
-
-
-    public void initPlaces(){
-        board[startRow][startCol] = 10;
-        board[endRow][endCol] = 20;
     }
 
     public void placeWalls(int [][] board){
@@ -53,8 +58,14 @@ public class PathFinder {
         board[5][6] = -1;
         board[6][6] = -1;
         board[7][6] = -1;
-        board[8][6] = -1;
 
+
+        board[9][2] = -1;
+        board[8][2] = -1;
+        board[6][2] = -1;
+        board[7][2] = -1;
+
+        board[5][2] = -1;
 
 
 
@@ -64,7 +75,7 @@ public class PathFinder {
     public static void displayArray(int[][] a) {
         for (int[] row : a) {
             for (int x : row) {
-                if(x == 0){
+                if(x < 10 && x >= 0){
                     System.out.print(" ");
                 }
                 System.out.print(x + " ");
@@ -77,7 +88,7 @@ public class PathFinder {
 
     }
 
-    //gives the path using A* search
+    //gives the path using A* search WORKING!
     public void A_Star_Search(){
 
         int [][] board = copyArray(this.board);
@@ -101,23 +112,16 @@ public class PathFinder {
         while(!(fcostHeader.element.row == endRow && fcostHeader.element.col == endCol) && canBeFound){
 
             //displayArray(board);
-
             //gets a list of possible places we can move from our current position
             ArrayList<Node> possibleNodes = generateNewNodesForAStar(fcostHeader,  endRow, endCol, board);
-
-
             fCostList.head = fCostList.head.next; //pop out the node
-
             for(Node n : possibleNodes){
                 fCostList.append(n); //appends the places in an order maintained way
 
             }
-            fCostList.print();
             fcostHeader = fCostList.head; //keep going
             fCostList._size--;
             if(fCostList._size == 1){
-                fCostList.print();
-                System.out.println(fCostList._size);
                 canBeFound = false;
             }
 
@@ -125,10 +129,10 @@ public class PathFinder {
         }
 
         if(!canBeFound){
-            System.out.println("Path cannot be found");
+            System.out.println("A* says path cannot be found");
         }
         else{
-            int counter = 1;
+            int counter = 0;
 
             System.out.println("A* predicts the best path is as follows: ");
             //if we reached here, we found the end, and we are going to retrace our steps
@@ -144,117 +148,6 @@ public class PathFinder {
         }
 
     }
-
-    //gives the path using Depth First Search
-
-    public void DFS(){
-
-        int [][] board = copyArray(this.board);
-        placeWalls(board);
-        board[startRow][startCol] = 10;
-        board[endRow][endCol] = 20;
-        Stack nodeStack = new Stack();
-        LocNode startingLocation = new LocNode(startRow, startCol, 0);
-        nodeStack.push(startingLocation);
-        boolean isNodeStackEmpty = false;
-        //until our locNode stack gives us the location
-        while(!(((LocNode)nodeStack.peek()).row ==  endRow && ((LocNode)nodeStack.peek()).col ==  endCol) &&  !isNodeStackEmpty){
-            //displayArray(board);
-
-
-            LocNode previousLocationNode = (LocNode) nodeStack.peek();
-            ArrayList<LocNode> locations = generateNewNodesForDFS(previousLocationNode,  board);
-            nodeStack.pop();
-            LocNode targetNode = null;
-            for(LocNode locationNode: locations){
-
-                if(!nodeStack.empty() && (((LocNode)nodeStack.peek()).row ==  endRow && ((LocNode)nodeStack.peek()).col ==  endCol)){
-
-                    System.out.println("TargetNode found");
-                    targetNode = (LocNode)nodeStack.peek();
-                }
-                else{
-                    nodeStack.push(locationNode);
-                }
-
-            }
-            //if the final destination is in the loc node list, then it needs to be at the end
-            if(targetNode!= null){
-                nodeStack.push(targetNode);
-            }
-            if(nodeStack.empty()){
-                isNodeStackEmpty = true;
-            }
-        }
-
-        if(isNodeStackEmpty){
-            System.out.println("Path can't be found with DFS");
-        }
-        else{
-            LocNode current = (LocNode) nodeStack.peek();
-            int counter = 0;
-            System.out.println("DFS Predicts the best path is as follows: ");
-            while( current != null){
-                System.out.println(current.row + " , " + current.col);
-                board[current.row][current.col] = 4;
-                counter++;
-                current = current.previous;
-            }
-            displayArray(board);
-            System.out.println("# of Moves : "   + counter);
-        }
-
-
-    }
-
-    public ArrayList<LocNode> generateNewNodesForDFS(LocNode previousLocation,  int [][] board){
-        int row = previousLocation.row;
-        int col = previousLocation.col;
-        int [] rowAdditions = {-1,0,1}; //this will gives 8 possible moves around the array
-        int [] colAdditions = {-1,0,1};
-        ArrayList<Dimension> moveGenerator = new ArrayList<>();
-        for(int rowAddition: rowAdditions){
-            for(int colAddition: colAdditions){
-                if(!(rowAddition == 0 && colAddition == 0)){
-                    moveGenerator.add(new Dimension(row + rowAddition, col + colAddition));
-                }
-
-            }
-        }
-        ArrayList<LocNode> validLocationGenerator = new ArrayList<>();
-        for (int i = 0; i < moveGenerator.size(); i++){
-
-            //check whether these moves are all valid
-            boolean isNodeValid = true;
-            int moveRow = moveGenerator.get(i).row;
-            int moveCol = moveGenerator.get(i).col;
-
-            if(moveRow < 0 || moveRow >= board.length){
-                isNodeValid = false;
-            }
-            if(moveCol < 0 || moveCol >= board[0].length){
-                isNodeValid = false;
-            }
-
-
-            if(isNodeValid){
-
-                if(!(board[moveRow][moveCol] == 10 || board[moveRow][moveCol] <0 )) {
-                    LocNode l = new LocNode(moveRow, moveCol, previousLocation.size + 1);
-                    l.previous = previousLocation; //attach the previousNode's location in our "move history"
-                    board[moveRow][moveCol] = -5;
-                    //find gCost and generate new node
-
-                    validLocationGenerator.add(l);
-                }
-
-            }
-        }
-        return validLocationGenerator;
-    }
-
-
-
 
     public ArrayList<Node> generateNewNodesForAStar(Node previousNode, int eRow, int eCol, int [][] board){
         LocNode location = previousNode.element;
@@ -291,10 +184,12 @@ public class PathFinder {
 
             if(isNodeValid){
 
-                if(!(board[moveRow][moveCol] == 10 || board[moveRow][moveCol] <0 )) {
+                if(!(board[moveRow][moveCol] == 10 || board[moveRow][moveCol] <0 || board[moveRow][moveCol] == 1)) {
                     LocNode l = new LocNode(moveRow, moveCol, previousNode.element.size);
                     l.previous = previousNode.element; //attach the previousNode's location in our "move history"
-                    board[moveRow][moveCol] = -5;
+                    board[row][col] = -5; // "pop" out the node we created calculations in
+                    board[moveRow][moveCol] = 1; //identify that we are looking at the nodes
+
                     //find gCost and generate new node
                     double gCost = previousNode.gCost + 10 * Math.sqrt(Math.pow(moveRow - row, 2) + Math.pow(moveCol - col, 2));
                     Node newNode = new Node(l, eRow, eCol, gCost);
@@ -305,6 +200,184 @@ public class PathFinder {
         }
 
         return validNodeGenerator;
+    }
+
+    //gives the path using Depth First Search Still Implementing new Ideas
+    // maybe better to use recursion
+    public void DFS(){
+
+        int [][] board = copyArray(this.board);
+        placeWalls(board);
+        board[startRow][startCol] = 10;
+        board[endRow][endCol] = 20;
+        Stack<LocNode> nodeStack = new Stack();
+        LocNode startingLocation = new LocNode(startRow, startCol, 0);
+        LocNode finalNode = null;
+        nodeStack.push(startingLocation);
+        boolean isNodeStackEmpty = false;
+        boolean foundEndPoint = false;
+        //until our locNode stack gives us the location
+        while(/*!(((LocNode)nodeStack.peek()).row ==  endRow && ((LocNode)nodeStack.peek()).col ==  endCol) && */  !nodeStack.empty()){
+
+            if(foundEndPoint){
+                if(finalNode == null){
+                    System.out.println("Changing Node from Null");
+                    finalNode = nodeStack.peek();
+                }
+                else if(finalNode.size > nodeStack.peek().size){
+                    System.out.println("Changed Node");
+                    System.out.println("Optimal Size : " + nodeStack.peek().size);
+                    finalNode = nodeStack.peek();
+                }
+                foundEndPoint = false;
+                nodeStack.pop();
+
+            }
+            else{
+                LocNode previousLocationNode = nodeStack.peek();
+                ArrayList<LocNode> locations = generateNewNodesForTreeSearch(previousLocationNode,  board, "DFS");
+                nodeStack.pop();
+                LocNode targetNode = null;
+                for(LocNode locationNode: locations){
+                    if(!nodeStack.empty() && (nodeStack.peek().row ==  endRow && nodeStack.peek().col ==  endCol)){
+                        System.out.println("TargetNode found, size :  " + nodeStack.peek().size);
+
+                        foundEndPoint = true;
+                        targetNode = nodeStack.peek();
+                    }
+                    else{
+                        nodeStack.push(locationNode);
+                    }
+
+                }
+                //if the final destination is in the loc node list, then it needs to be at the end
+                if(targetNode != null){
+                    nodeStack.push(targetNode);
+                }
+            }
+
+
+        }
+
+        if(isNodeStackEmpty){
+            System.out.println("Path can't be found with DFS");
+        }
+        else{
+            LocNode current = finalNode; //nodeStack.peek();
+            int counter = 0;
+            System.out.println("DFS Predicts the best path is as follows: ");
+            while( current != null){
+                System.out.println(current.row + " , " + current.col);
+                board[current.row][current.col] = 4;
+                counter++;
+                current = current.previous;
+            }
+            System.out.println("# of Moves : "   + counter);
+            displayArray(board);
+        }
+
+
+    }
+
+    //gives the path using Breadth First Search WORKING!
+    public void BFS(){
+        int [][] board = copyArray(this.board);
+        placeWalls(board);
+        board[startRow][startCol] = 10;
+        board[endRow][endCol] = 20;
+        LinkedList <LocNode> nodeQueue = new LinkedList<>();
+        nodeQueue.addFirst(new LocNode(startRow, startCol, 0));
+
+        while(!(nodeQueue.getLast().row == endRow && nodeQueue.getLast().col == endCol)){
+            //displayArray(board);
+            //System.out.println("--------------------");
+            //System.out.println(nodeQueue);
+            LocNode endNode = nodeQueue.getLast();
+            ArrayList<LocNode> possibleLocNodes = generateNewNodesForTreeSearch(endNode, board, "BFS");
+            //dequeue the final node as we have searched possible nodes from it
+            nodeQueue.removeLast();
+
+            for(LocNode node: possibleLocNodes){
+                nodeQueue.addFirst(node);
+            }
+            if(nodeQueue.isEmpty()){
+                break;
+            }
+        }
+
+        if(nodeQueue.isEmpty()){
+            System.out.println("BFS couldn't find a path");
+        }
+        else{
+            LocNode endNode = nodeQueue.getLast();
+            System.out.println("BFS predicts the path is: ");
+            int counter = 0;
+            while(endNode != null){
+                System.out.println(endNode.row + " , " + endNode.col);
+                board[endNode.row][endNode.col] = 4;
+                endNode = endNode.previous;
+                counter++;
+            }
+            displayArray(board);
+            System.out.println("# of Moves: " + counter);
+        }
+
+    }
+
+    //gets potential nodes for tree based searching algorithms
+    public ArrayList<LocNode> generateNewNodesForTreeSearch(LocNode previousLocation,  int [][] board, String whichSearch){
+        boolean dontConsiderOnes = false;
+        if(whichSearch.equals("BFS")){
+            dontConsiderOnes = true;
+        }
+        int row = previousLocation.row;
+        int col = previousLocation.col;
+
+
+        int [] rowAdditions = {-1,0,1}; //this will gives 8 possible moves around the array
+        int [] colAdditions = {-1,0,1};
+        ArrayList<Dimension> moveGenerator = new ArrayList<>();
+        for(int rowAddition: rowAdditions){
+            for(int colAddition: colAdditions){
+                if(!(rowAddition == 0 && colAddition == 0)){
+                    moveGenerator.add(new Dimension(row + rowAddition, col + colAddition));
+                }
+
+            }
+        }
+        ArrayList<LocNode> validLocationGenerator = new ArrayList<>();
+        for (int i = 0; i < moveGenerator.size(); i++){
+
+            //check whether these moves are all valid
+            boolean isNodeValid = true;
+            int moveRow = moveGenerator.get(i).row;
+            int moveCol = moveGenerator.get(i).col;
+
+            if(moveRow < 0 || moveRow >= board.length){
+                isNodeValid = false;
+            }
+            if(moveCol < 0 || moveCol >= board[0].length){
+                isNodeValid = false;
+            }
+
+
+            if(isNodeValid){
+
+                if(!(board[moveRow][moveCol] == 10 || board[moveRow][moveCol] <0 )) {
+                    //dont append things that are already in the queue, as it is a waste of time and space
+                    if(!(dontConsiderOnes && board[moveRow][moveCol] == 1)){
+                        LocNode l = new LocNode(moveRow, moveCol, previousLocation.size + 1);
+                        l.previous = previousLocation; //attach the previousNode's location in our "move history"
+                        board[row][col] = -5;
+                        board[moveRow][moveCol] = 1; //the node is being examined
+                        validLocationGenerator.add(l);
+                    }
+
+                }
+
+            }
+        }
+        return validLocationGenerator;
     }
 
 }
@@ -386,10 +459,6 @@ class MinList {
 
 }
 
-
-
-
-
 //used in MinList
 class Node {
     LocNode element; //this is a location node
@@ -413,7 +482,6 @@ class Node {
         this.next = null;
         this.previous = null;
         this.gCost = gCost;
-        System.out.println("GCost : " + gCost);
         calculateCost(eRow, eCol);
         //compute the cost
     }
@@ -426,11 +494,17 @@ class LocNode{
     int size;
     LocNode previous; //this will used to retrace our steps
 
-    public LocNode(int r, int c, int size){
+    public LocNode(int r, int c, int s){
         row = r;
         col = c;
-        size = size;
+        size = s;
     }
+
+    @Override
+    public String toString(){
+        return "( " + col + " , " + row + " )";
+    }
+
 }
 
 class Dimension{
@@ -441,3 +515,4 @@ class Dimension{
         col = c;
     }
 }
+
